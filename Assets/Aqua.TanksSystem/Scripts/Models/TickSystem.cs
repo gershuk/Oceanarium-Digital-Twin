2a6ff9ae44586081a780e7b743cc65102b41586e
Vladislav Gershuk <vladik.morozov.2012@mail.ru>
2023-10-27 03:03:09 +1000

    Add tick system.

---
+++ Assets/Aqua.TanksSystem/Scripts/Models/TickSystem.cs
@@ -0,0 +1,57 @@
+#nullable enable
+
+using System;
+using System.Collections.Generic;
+
+using UnityEngine;
+
+namespace Aqua.TanksSystem
+{
+    public interface ITickObject
+    {
+        public void Init (float startTime);
+        public void Tick (int tickNumber, float startTime, float tickTime);
+    }
+
+    public sealed class TickSystem : MonoBehaviour
+    {
+        [SerializeField]
+        private LinkedList<ITickObject> _tickObjectQueue = new();
+
+        [SerializeField]
+        private float _startTime = 0;
+
+        [SerializeField]
+        private float _tickTime;
+
+        private int _tickNumber = 0;
+
+        private void Start ()
+        {
+            foreach (var tickObject in _tickObjectQueue)
+            {
+                tickObject.Init(_startTime);
+            }
+        }
+
+        private void Update ()
+        {
+            foreach (var tickObject in _tickObjectQueue)
+            {
+                tickObject.Tick(_tickNumber, _startTime, _tickTime);
+            }
+
+            _tickNumber++;
+        }
+
+        public void AddToEnd (ITickObject tickObject)
+        {
+            _tickObjectQueue.AddLast(tickObject);
+        }
+
+        public void Remove (ITickObject tickObject)
+        {
+            _tickObjectQueue.Remove(tickObject);
+        }
+    }
+}
